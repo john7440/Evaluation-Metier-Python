60b0d9cfc762ac0bb59b883e95b9696a698d7b91
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 11:18:48 +0100

    modifications des noms d'attribut pour coller à la bdd

--- goncourt/models/book.py
+++ goncourt/models/book.py
@@ -4,15 +4,19 @@ from datetime import date
 
 @dataclass
 class Book:
-    title: str
-    publication_date: date
-    number_of_pages: int
-    summary: Optional[str] = None
-    isbn: Optional[str] = None
-    pages: Optional[float] = None
+    b_title: str
+    b_publicationDate: date
+    b_pagesNb: int
+    Id_Book: Optional[int] = None
+    b_summary: Optional[str] = None
+    b_isbn: Optional[str] = None
+    b_price: Optional[float] = None
 
-    author: Optional["Author"] = None  # type: ignore
+    Id_Author: Optional["Author"] = None # type: ignore
+    Id_Publisher: Optional["Publisher"] = None # type: ignore
     selections: List["Selection"] = field(default_factory=list)  # type: ignore
+    characters: List["Character"] = field(default_factory=list) # type: ignore
+    votes: List["Vote"] = field(default_factory=list) # type: ignore
 
     def __str__(self):
-        return f"{self.title} ({self.publication_date.year})"
+        return f"{self.b_title}, ({self.b_publicationDate.year})"
