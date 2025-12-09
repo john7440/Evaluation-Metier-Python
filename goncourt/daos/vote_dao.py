9196800660b47d7091e308a945ac0edd5a20d6ff
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 13:19:53 +0100

    ajout de la classe dao des votes + read et read all

---
+++ goncourt/daos/vote_dao.py
@@ -0,0 +1,65 @@
+from typing import List, Optional
+import pymysql
+
+from goncourt.daos.dao import Dao, T
+from goncourt.models.vote import Vote
+
+class VoteDAO(Dao[Vote]):
+    def create(self, obj: T) -> int:
+        """Créer un vote"""
+        pass
+
+    def read(self, id_entity: int) -> Optional[Vote]:
+        try:
+            with self.connection.cursor() as cursor:
+                sql = """
+                            SELECT Id_Vote, number_vote, Id_Book, Id_JuryMember
+                            FROM vote
+                            WHERE Id_Vote = %s
+                        """
+                cursor.execute(sql, (id_entity,))
+                row = cursor.fetchone()
+                if row:
+                    return Vote(
+                        id_vote=row["Id_Vote"],
+                        number_vote=row["number_vote"],
+                        id_book=row["Id_Book"],
+                        id_jury_member=row["Id_JuryMember"]
+                    )
+                return None
+        except pymysql.MySQLError as e:
+            print(f"Erreur lors du read Vote: {e}")
+            return None
+
+
+    def read_all(self) -> List[Vote]:
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT Id_Vote, number_vote, Id_Book, Id_JuryMember FROM vote"
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                votes = []
+                for row in rows:
+                    votes.append(
+                        Vote(
+                            id_vote=row["Id_Vote"],
+                            number_vote=row["number_vote"],
+                            id_book=row["Id_Book"],
+                            id_jury_member=row["Id_JuryMember"]
+                        )
+                    )
+                return votes
+        except pymysql.MySQLError as e:
+            print(f"Erreur lors du read_all de Vote: {e}")
+            return []
+
+    def update(self, obj: T) -> bool:
+        """Update un vote"""
+        pass
+
+    def delete(self, obj: T) -> bool:
+        """Supprimer un vote"""
+        pass
+
+
+
--- goncourt/main.py
+++ goncourt/main.py
@@ -1,5 +1,15 @@
+from goncourt.daos.author_dao import AuthorDao
+from goncourt.daos.book_dao import BookDao
+from goncourt.daos.jury_member_dao import JuryMemberDao
+from goncourt.daos.publisher_dao import PublisherDao
+from goncourt.daos.selection_dao import SelectionDao
+
+
 def main():
-    pass
+    dao = SelectionDao()
+    authors = dao.read_all()
+    for author in authors:
+        print(author)
 
 if __name__ == "__main__":
     main()
