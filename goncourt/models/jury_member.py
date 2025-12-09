3bf42ba1229dd0b9c071b25e59538dfa63a10ca4
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 09:22:44 +0100

    ajout d'une classe pour les memebre du jury avec un enum pour différencier les membres du président

---
+++ goncourt/models/jury_member.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+from goncourt.models.person import Person
+
+class JuryRole(Enum):
+    PRESIDENT = "Président"
+    MEMBER = "Membre"
+
+@dataclass
+class JuryMember(Person):
+    role: JuryRole
+    login: Optional[str] = None
+    password: Optional[str] = None
+
+    def get_role(self) -> str:
+        return self.role.value
