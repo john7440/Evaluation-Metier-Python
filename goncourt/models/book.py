fe9bff2c1e994287f2f4d0f36cf68a49dad4718e
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 09:37:41 +0100

    ajout de la classe book

---
+++ goncourt/models/book.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from typing import Optional, List
+from datetime import date
+
+@dataclass
+class Book:
+    title: str
+    publication_date: date
+    number_of_pages: int
+    summary: Optional[str] = None
+    isbn: Optional[str] = None
+    pages: Optional[float] = None
+
+    author: Optional["Author"] = None  # type: ignore
+    selections: List["Selection"] = field(default_factory=list)  # type: ignore
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_date.year})"
