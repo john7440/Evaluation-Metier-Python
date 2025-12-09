2f3c3d811fb4cf5e1c858b9c3daaabab89ff73f6
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 11:36:13 +0100

    ajout de la classe dao des auteur avec read et read all

---
+++ goncourt/daos/author_dao.py
@@ -0,0 +1,55 @@
+from typing import List, Optional
+import pymysql
+
+from goncourt.daos.dao import T
+from goncourt.models.author import Author
+from goncourt.daos.dao import Dao
+
+class AuthorDao(Dao[Author]):
+    def delete(self, obj: T) -> bool:
+        pass
+
+    def update(self, obj: T) -> bool:
+        pass
+
+    def create(self, obj: T) -> int:
+        pass
+
+    def read(self, id_entity: int) -> Optional[Author]:
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT Id_Author, a_first_name, a_last_name, a_biography FROM author WHERE Id_Author = %s"
+                cursor.execute(sql, (id_entity,))
+                row = cursor.fetchone()
+                if row:
+                    return Author(
+                        id_author=row["Id_Author"],
+                        first_name=row["a_first_name"],
+                        last_name=row["a_last_name"],
+                        biography=row["a_biography"]
+                    )
+                return None
+        except pymysql.MySQLError as e:
+            print(f"Erreur lors du read Author: {e}")
+            return None
+
+    def read_all(self) -> List[Author]:
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT Id_Author, a_first_name, a_last_name, a_biography FROM author"
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                authors = []
+                for row in rows:
+                    authors.append(
+                        Author(
+                            id_author=row["Id_Author"],
+                            first_name=row["a_first_name"],
+                            last_name=row["a_last_name"],
+                            biography=row["a_biography"]
+                        )
+                    )
+                return authors
+        except pymysql.MySQLError as e:
+            print(f"Erreur lors du read_all Author: {e}")
+            return []
