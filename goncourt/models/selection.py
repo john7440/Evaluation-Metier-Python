24b216282f7776568a59f45d5922b7a2ad61354a
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-09 09:58:57 +0100

    ajout de la classe selection

---
+++ goncourt/models/selection.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from typing import  List
+from datetime import date
+
+from goncourt.models.book import Book
+
+"""Classe qui représente les différente sélection avec leur numéro, date et la liste des livres"""
+
+@dataclass
+class Selection:
+    date_selection: date
+    number_selection: int
+
+    # Une sélection contient plusieurs livres
+    books: List[Book] = field(default_factory=list)
+
+    def __str__(self):
+        return f"Sélection {self.number_selection} ({self.date_selection})"
