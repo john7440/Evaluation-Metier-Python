3b9fa0888164fd06331d727b7903bad82f57492f
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-10 19:44:54 +0100

    ajout de commentaires

--- goncourt/models/author.py
+++ goncourt/models/author.py
@@ -5,8 +5,16 @@ from goncourt.models.person import Person
 
 @dataclass
 class Author(Person):
+    """
+    Classe représentant un auteur, héritée de Person
+
+    Attributs :
+    : id_author: (Optional[int]) : identifiant unique de l'auteur (optionnel)
+    : biography: (Optional[str]) : texte biographique de l'auteur (optionnel)
+    """
     id_author: Optional[int] = None
     biography: Optional[str] = None
 
     def get_role(self) -> str:
+        """ Retourne le role associé à cette classe """
         return 'Author'
