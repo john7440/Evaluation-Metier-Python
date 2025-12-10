00bd4ba8deb4ce44fcec875e666e63f03bf1c482
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-10 08:44:19 +0100

    ajout d'une fonction utilitaire pour chercher les livre par leur titre

---
+++ goncourt/business/goncourt.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Goncourt
+"""
+from typing import List
+import pymysql
+
+from goncourt.daos.author_dao import AuthorDao
+from goncourt.daos.selection_dao import SelectionDao
+from goncourt.daos.vote_dao import VoteDao
+from goncourt.daos.book_dao import BookDao
+from goncourt.daos.publisher_dao import PublisherDao
+from goncourt.models.book import Book
+
+
+def search_by_title(self, title: str) -> List[Book]:
+    """Rechercher des livres par titre """
+    try:
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM book WHERE b_title LIKE %s"
+            cursor.execute(sql, (f"%{title}%",))
+            rows = cursor.fetchall()
+            return [Book(**row) for row in rows]
+    except pymysql.MySQLError as e:
+        print(f"Erreur search_by_title: {e}")
+        return []
+
