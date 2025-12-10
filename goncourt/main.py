77d9250b1d25a133b90105fdcd4726bc2a341080
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-10 10:36:56 +0100

    instanciation + lancement du menu dans le main

--- goncourt/main.py
+++ goncourt/main.py
@@ -1,15 +1,15 @@
-from goncourt.daos.author_dao import AuthorDao
 from goncourt.daos.book_dao import BookDao
-from goncourt.daos.jury_member_dao import JuryMemberDao
-from goncourt.daos.publisher_dao import PublisherDao
 from goncourt.daos.selection_dao import SelectionDao
-
+from goncourt.daos.vote_dao import VoteDao
+from goncourt.menu import Menu
 
 def main():
-    dao = SelectionDao()
-    authors = dao.read_all()
-    for author in authors:
-        print(author)
+    menu = Menu(
+        book_dao=BookDao(),
+        selection_dao=SelectionDao(),
+        vote_dao=VoteDao()
+    )
+    menu.run()
 
 if __name__ == "__main__":
     main()
